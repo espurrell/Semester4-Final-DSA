91ac74bb34570d67668ee8ed5715cc734d592d93
espurrell <150044207+espurrell@users.noreply.github.com>
2024-11-28 13:57:50 -0330

    Corrected file structure

--- src/main/java/com/keyin/bstapp/controller/TreeController.java
+++ src/main/java/com/keyin/bstapp/controller/TreeController.java
@@ -2,7 +2,7 @@ package com.keyin.bstapp.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.keyin.bstapp.entity.TreeEntity;
+
 import com.keyin.bstapp.service.TreeService;
 import com.keyin.bstapp.trees.BinaryNode;
 import org.springframework.beans.factory.annotation.Autowired;
